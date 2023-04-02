870978e3939b77cc5fdacc1a0dc5834880d8ac11
Haikhal Fakhreez <ekaliacid@Ekal-MacBook-Pro.local>
2023-04-02 23:08:00 +0800

    fix next

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,16 @@
-import { Poppins } from "@next/font/google";
-import clsx from "clsx";
-import Image from "next/image";
-import Github from "../components/GitHub";
+import { Poppins } from 'next/font/google'
+import clsx from 'clsx'
+import Image from 'next/image'
+import Github from '../components/GitHub'
 
-const poppins = Poppins({ weight: "800", subsets: ["latin"] });
+const poppins = Poppins({ weight: '800', subsets: ['latin'] })
 
 export default function Header() {
   return (
     <div className="flex items-center justify-between px-3 sm:px-3">
-      <a
-        className="flex items-center space-x-3"
-        href="https://news-summarizer.vercel.app/"
-      >
+      <a className="flex items-center space-x-3" href="https://news-summarizer.vercel.app/">
         <Image src="/edit.png" alt="logo" width={34} height={34} />
-        <h2 className={clsx("text-lg sm:text-3xl", poppins.className)}>
+        <h2 className={clsx('text-lg sm:text-3xl', poppins.className)}>
           <span className="text-green-500">News</span> summarizer
         </h2>
       </a>
@@ -26,5 +23,5 @@ export default function Header() {
         <Github width="33" height="33" />
       </a>
     </div>
-  );
+  )
 }
